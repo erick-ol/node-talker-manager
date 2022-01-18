6e112f1c48186a3aa270bcef2fadd26f056b7ab9
Erick Santos <erickosantos.dev@gmail.com>
2022-01-18 16:03:41 -0300

    1 - Crie o endpoint GET /talker

--- index.js
+++ index.js
@@ -15,3 +15,18 @@ app.get('/', (_request, response) => {
 app.listen(PORT, () => {
   console.log('Online');
 });
+
+// leitura arquivo talker.json
+const fs = require('fs').promises;
+
+const getTalkers = async () => {
+  const fileContent = await fs.readFile('./talker.json');
+  const talkers = JSON.parse(fileContent);
+  return talkers;
+};
+
+app.get('/talker', async (req, res) => {
+  const talkers = await getTalkers();
+  if (talkers.length === 0) res.status(HTTP_OK_STATUS).json([]);
+  else res.status(HTTP_OK_STATUS).json(talkers);
+});
